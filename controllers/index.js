09e4272346543eb61e9d8ebd17aaacee6ed6a316
leena <annachandy123@gmail.com>
2021-11-29 21:23:29 +1100

    vehicle_product_partials

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
+const vehicleRoutes = require("./vehicleRoutes");
 
 router.use("/", homeRoutes);
+router.use("/vehicle", vehicleRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
--- controllers/vehicleRoutes.js
+++ controllers/vehicleRoutes.js
@@ -1,75 +1,50 @@
-// const router = require("express").Router();
-// const { Categories, Electronics, Furniture, Misc, Property, Vehicle, User } = require("../models");
-// // Import the custom middleware
-// const withAuth = require("../utils/auth");
+const router = require("express").Router();
+const { Vehicle, User } = require("../models");
+// Import the custom middleware
+const withAuth = require("../utils/auth");
 
-// // GET all
-// router.get("/vehicle", async (req, res) => {
-//     try {
-//         const vehicleData = await Vehicle.findAll({
-//             // include: [
-//             //     {
-//             //         model: Property,
-//             //         attributes: ["", ""],
-//             //     },
-//             // ],
-//         });
+// GET all
+router.get("/vehicle", withAuth, async (req, res) => {
+    try {
+        const vehicleData = await Vehicle.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["username", "email"],
+                },
+            ],
+        });
 
-//         const Vehicle = vehicleData.map((vehicle) => vehicle.get({ plain: true }));
+        const vehicles = vehicleData.map((vehicle) => vehicle.get({ plain: true }));
 
-//         res.render("homepage", {
-//             Vehicle,
-//             loggedIn: req.session.loggedIn,
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
+        res.render("homepage", {
+            vehicles,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
-// // GET one gallery
-// // Use the custom middleware before allowing the user to access the gallery
-// router.get("/vehicle/:id", withAuth, async (req, res) => {
-//     try {
-//         const vehicleData = await Vehicle.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: Painting,
-//                     attributes: ["id", "title", "artist", "exhibition_date", "filename", "description"],
-//                 },
-//             ],
-//         });
+// Use the custom middleware before allowing the user to access the vehicle
+router.get("/vehicle/:id", withAuth, async (req, res) => {
+    try {
+        const vehicleData = await Vehicle.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ["username", "email"],
+                },
+            ],
+        });
 
-//         const gallery = dbGalleryData.get({ plain: true });
-//         res.render("gallery", { gallery, loggedIn: req.session.loggedIn });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
+        const vehicles = vehicleData.get({ plain: true });
+        res.render("vehicledata", { vehicles, logged_in: req.session.logged_in });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
-// // GET one painting
-// // Use the custom middleware before allowing the user to access the painting
-// router.get("/painting/:id", withAuth, async (req, res) => {
-//     try {
-//         const dbPaintingData = await Painting.findByPk(req.params.id);
-
-//         const painting = dbPaintingData.get({ plain: true });
-
-//         res.render("painting", { painting, loggedIn: req.session.loggedIn });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
-// });
-
-// router.get("/login", (req, res) => {
-//     if (req.session.loggedIn) {
-//         res.redirect("/");
-//         return;
-//     }
-
-//     res.render("login");
-// });
-
-// module.exports = router;
+module.exports = router;
