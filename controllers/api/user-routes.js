4aee3b9f6930e3599d6c8db5ac7cf3d99fdd7708
RobertCalder1 <robertcalder27@hotmail.com>
2021-11-29 19:32:34 +1100

    added check for existing username/password to account creation script

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,21 +3,32 @@ const { User } = require("../../models");
 
 // CREATE new user
 router.post("/", async (req, res) => {
-  try {
-    const dbUserData = await User.create({
+  const user = await User.findOne({
+    where: {
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
-    });
+    },
+  });
+  if (username || email) {
+    res.json({ error: true, Error: "Username/Email address already exists" });
+    return;
+  } else {
+    try {
+      const dbUserData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+      req.session.save(() => {
+        req.session.loggedIn = true;
 
-      res.status(200).json(dbUserData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+        res.status(200).json(dbUserData);
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
 });
 
