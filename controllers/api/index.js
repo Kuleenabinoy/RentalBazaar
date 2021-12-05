aa869b5194351d4b9471dd4322dd0c3f0b1b4201
8nd8 <88880649+8nd8@users.noreply.github.com>
2021-12-05 13:55:06 +1100

    API index.js consts and routers removed for old categories

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
-const electronicsRoutes = require("./electronics-routes");
 const categoriesRoutes = require("./categories-routes");
-const furnitureRoutes = require("./furniture-routes");
 const miscRoutes = require("./misc-routes");
 const propertyRoutes = require("./properties-routes");
 const vehicleRoutes = require("./vehicle-routes");
@@ -12,8 +10,7 @@ router.use("/users", userRoutes);
 router.use("/vehicle", vehicleRoutes);
 router.use("/categories", categoriesRoutes);
 router.use("/property", propertyRoutes);
-router.use("/electronics", electronicsRoutes);
 router.use("/misc", miscRoutes);
-router.use("/furniture", furnitureRoutes);
+
 
 module.exports = router;
