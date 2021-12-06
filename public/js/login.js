727cb86d554f4c3a1d1a699a7166cc02b2a24682
leena <annachandy123@gmail.com>
2021-12-06 12:54:06 +1100

    changes

--- public/js/login.js
+++ public/js/login.js
@@ -1,58 +1,24 @@
 const loginFormHandler = async (event) => {
-  event.preventDefault();
-
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
-
-  if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok && isadmin === true) {
-      document.location.replace("/admin-dashboard");
-      
-    } else if (response.ok) {
-      document.location.replace("/home");
-      
-    } else {
-      alert(
-        "Failed to log in. Please check your username and password and try again"
-      );
-    }
-};
-
-<<<<<<< HEAD
-const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const username = document.querySelector("#username-signup").value.trim();
-    const email = document.querySelector("#email-signup").value.trim();
-    const password = document.querySelector("#password-signup").value.trim();
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
 
-    if (username && email && password) {
-        const response = await fetch("/api/users", {
+    if (email && password) {
+        const response = await fetch("/api/users/login", {
             method: "POST",
-            body: JSON.stringify({ username, email, password }),
+            body: JSON.stringify({ email, password }),
             headers: { "Content-Type": "application/json" },
         });
 
-        if (response.ok) {
-            document.location.replace("/");
+        if (response.ok && isadmin === true) {
+            document.location.replace("/admin-dashboard");
+        } else if (response.ok) {
+            document.location.replace("/home");
         } else {
-            alert("Failed to sign up.");
+            alert("Failed to log in. Please check your username and password and try again");
         }
     }
+
+    document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
 };
-
-document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
-
-document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
-=======
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
-
->>>>>>> ef67557cdeaa946df234d6edc133864650db6118
